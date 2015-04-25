7a21cdebcbbbd99b9c4d091dfc89a23442981ce7
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-04-25 20:10:59 +0200

    Add Configuration for migration

---
+++ ChatterREST/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using ChatterREST.Models;
+
+namespace ChatterREST.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    }
+}
