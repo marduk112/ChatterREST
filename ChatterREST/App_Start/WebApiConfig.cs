fe716fd08aa81325c335ba0035afbce3d14545fa
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-05-22 17:54:14 +0200

    User must authorize to get Bet data

--- ChatterREST/App_Start/WebApiConfig.cs
+++ ChatterREST/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace ChatterREST
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-            //config.Filters.Add(new AuthorizeAttribute());
+            config.Filters.Add(new AuthorizeAttribute());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
